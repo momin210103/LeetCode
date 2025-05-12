fe4d23891704f964a4f0294d32184aad45eb0402
momin210103 <momincse13@gmail.com>
2025-05-12 10:10:26 +0600

    Time: 0 ms (100%), Space: 12.1 MB (34.87%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==0) return "";
+        string pre = strs[0];
+        for(int i =1; i<strs.size(); i++){
+            while(strs[i].find(pre)!=0){
+                pre =  pre.substr(0,pre.length()-1);
+                if(pre.empty()) return "";
+            }
+
+        }
+        return pre;
+        
+    }
+};
