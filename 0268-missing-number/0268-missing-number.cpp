9032d4c4a43a8fe53b451f316dc3c4bba48c65e4
Momin Sheikh <momincse13@gmail.com>
2025-05-26 10:42:15 +0600

    Time: 3 ms (20.77%), Space: 22.1 MB (10.99%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
+    
 public:
+
     int missingNumber(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
         int len = nums.size();
         int val1 = 0;
         int val = accumulate(nums.begin(),nums.end(),0);
         for(int i=0; i<=len; i++){
             val1 += i;
         }
-        
-        
         return val1-val;
         
     }
