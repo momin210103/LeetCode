24ae7ac1f511742eff7a6f7f5512553a80505264
Momin Sheikh <momincse13@gmail.com>
2025-05-28 18:21:42 +0600

    Time: 7 ms (10.92%), Space: 14.9 MB (8.82%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        set<int>st(nums1.begin(),nums1.end());
+        set<int>st2(nums2.begin(),nums2.end());
+        for(auto i:st){
+            if(st2.find(i)!=st2.end()){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+        
+    }
+};
