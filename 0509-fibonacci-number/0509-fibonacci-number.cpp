f5bf331d35556b25d6ce30d5e7c3f0c699803954
Momin Sheikh <momincse13@gmail.com>
2025-09-11 12:45:52 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1){
-            return n;
-        }
-        vector<int>fibs(n+1);
-        fibs[0] = 0;
-        fibs[1] = 1;
-        for(int i=2; i<=n; i++){
-            fibs[i] = fibs[i-1]+fibs[i-2];
-        }
-        return fibs[n];
+        if(n==1 || n==2) return 1;
+        return fib(n-1) + fib(n-2);
         
     }
 };
