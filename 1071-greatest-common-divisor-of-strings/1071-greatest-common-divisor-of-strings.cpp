5b833cd6ba13474ccae20c7d24978ac5fe786e09
Momin Sheikh <momincse13@gmail.com>
2025-10-10 11:07:38 +0600

    Time: 3 ms (22.36%), Space: 9.9 MB (30.44%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2!=str2+str1){
+            return "";
+        }
+        int g = gcd(str1.size(),str2.size());
+        string str = str1.substr(0,g);
+        return str;
+        
+    }
+};
